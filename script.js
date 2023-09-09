fe33595604879837ffba1426cfc872fc5676812b
Auridebson Lima <auridebson@vutus.com>
2023-09-09 17:46:40 -0300

    Update

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
+const nomeAluno = document.getElementById("nomeAluno")
+const faixaAluno = document.querySelector("faixaAluno")
+const datNascimento = document.querySelector("datNascimento")
 
-function arredonda() {
-    const cpNum1 = document.querySelector('#num1').value
-    const cpNum2 = document.querySelector('#num2').value
-
-    if (cpNum1,cpNum2 > 0) {
-        const cpRes = cpNum2/cpNum1 
-        document.querySelector('#numResultado').value = Math.round(cpRes)
-    }
-
+function coletar() {
+    alert(nomeAluno.textContent)
 }
-
-
-
-
