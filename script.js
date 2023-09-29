b3644b669df1d03f7f5b6e0859867fdcd7dc4158
Auridebson Sousa <auridebson@vutus.com>
2023-09-29 19:54:07 -0300

    Update

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-
 const btnAciona = document.querySelector(".btnAciona")
+const containerName = document.querySelector(".container-name")
 
-btnAciona.addEventListener("click", mostraMsg)
+const alertaEF = () => alert("Mostrando mensagem\nCom Expressão de função - Arrow")
+const dt = new Date()
+const infoData = () => alert(`A data coletada nessa Arrow Function é\n${dt.getDay()}/${dt.getMonth()}/${dt.getFullYear()}`)
+// const nomeUser = prompt("Digite seu mome")
 
-function mostraMsg() {
-    alert("Mostrando mensagem")
+const infoNome = () => {
+    return containerName.innerHTML += nomeUser + '<br>'
 }
+
+
+function alertaDF() {
+    alert("Mostrando mensagem\nCom Declaração de função")
+}
+
+
+
+btnAciona.addEventListener("click", infoNome)
