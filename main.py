cdb6e3c662178a873d70efd81728d855fa325b57
Auridebson Lima <auridebson@vutus.com>
2023-11-03 09:57:02 -0300

    Update

---
+++ main.py
@@ -0,0 +1,14 @@
+def ln(x):
+    print("-"*x)
+
+numeros = []
+numeros += input("Digite a primeira nota: ")
+numeros += input("Digite a segunda nota: ")
+numeros += input("Digite a terceira nota: ")
+numeros += input("Digite a quarta nota: ")
+
+
+
+
+
+print(min(numeros))
